369b38865e15cb1ab6875c3ba29945892c1d3ec4
iegorov.oleg <iegorov.oleg@01262fc2-a5f8-4dc7-5c05-deaa4208ba9e>
2011-02-20 11:59:25 +0000

    working with Triangle

--- trunk/triangle.h
+++ trunk/triangle.h
@@ -13,16 +13,28 @@ public :
   virtual ~Triangle() {};
   
   // Accessors
-  float* getFirst() const { return first_; }
-  void setFirst(float *first) { first_ = first; }
+  float* getFirst() { return first_; }
+  void setFirst(float *first) { 
+    first_[0] = first[0];
+    first_[1] = first[1];
+    first_[2] = first[2];
+  }
   
-  float* getSecond() const { return second_; }
-  void setSecond(float *second) { second_ = second; }
+  float* getSecond() { return second_; }
+  void setSecond (float *second) { 
+    second_[0] = second[0];
+    second_[1] = second[1];
+    second_[2] = second[2];
+  }
   
-  float* getThird() const { return third_; }
-  void setThird(float *third) { third_ = third; }
+  float* getThird() { return third_; }
+  void setThird (float *third) { 
+    third_[0] = third[0];
+    third_[1] = third[1];
+    third_[2] = third[2];
+  }
   
-  virtual float boundingRadius() const { return height(); }
+  virtual float boundingRadius() const { return 1.0; }
   
   virtual bool intersect(const Ray&, Hit& hit) const;
 
@@ -32,7 +44,7 @@ public :
 private:
 
   //Coordinates of the triangle 
-  float first[3];
-  float second[3];
-  float third[3];
-}
+  float first_[3];
+  float second_[3];
+  float third_[3];
+};
