efbdb7fdc45e12360ea09232116721185c7746cd
iegorov.oleg <iegorov.oleg@01262fc2-a5f8-4dc7-5c05-deaa4208ba9e>
2011-04-28 07:33:50 +0000

    meeerge shiiit

--- TP7/object.h
+++ TP7/object.h
@@ -4,61 +4,91 @@
 #include "material.h"
 #include "hit.h"
 #include "ray.h"
-
+#include "QGLViewer/quaternion.h"
+#include <string>
 
 #ifndef fmax
 #define fmax max
 #endif
 
-// Classe générique abstraite dont vont deriver les objets de la scene.
-class Object
-{
-public :
-  virtual ~Object() {};
-  
-  //type for key Frames, a pair of <time, Frame>
-  typedef std::pair<float, qglviewer::Frame> KeyFrame;
+// Classe gï¿½nï¿½rique abstraite dont vont deriver les objets de la scene.
 
-  //key frame smart array
-  std::list<KeyFrame> keyframe_;
-  
-  // Accesseurs
-  //
-  const Material& material() const { return material_; }
-  const qglviewer::Frame& frame() const { return frame_; }
+class Object {
+public:
 
-  // Modifieurs
-  //
-  void setMaterial(const Material& material) { material_ = material; }
-  void setFrame(const qglviewer::Frame& frame) { frame_ = frame; }
+    virtual ~Object() {
+    };
 
-  // Affichage openGL de l'objet
-  //
-  virtual void draw() const = 0;
-  virtual void animate(float time);
+    //type for key Frames, a pair of <time, Frame>
+    typedef std::pair<float, qglviewer::Frame> KeyFrame;
 
-  // Rayon d'une sphère englobante (meme grossiere).
-  //
-  virtual float boundingRadius() const = 0;
+    //key frame smart array
+    std::list<KeyFrame> anglekeyframe_;
+    std::list<KeyFrame> translationkeyframe_;
+    
 
-  // Intersection method between a Ray and the Object. The resulting
-  // intersection (if any) is stored in the Hit. The associated Frame
-  // allow to transform from local coordinates of this node to global
-  // coordinates.
-  //
-  virtual bool intersect(const Ray&, Hit& hit, qglviewer::Frame frame) const = 0;
 
-  virtual void initFromDOMElement(const QDomElement& e);
 
-  // This operator allow to concatenate two transformations. It performs
-  // the multiplication of the two transformation matrices in the order of
-  // the arguments.
-  //
-  qglviewer::Frame MultiplyFrame(const qglviewer::Frame& f1, const qglviewer::Frame& f2) const;
+    // Accesseurs
+    //
 
-protected :
-  // Tous les objets ont un repere et un materiau.
-  //
-  qglviewer::Frame frame_;
-  Material material_;
+    const Material& material() const {
+        return material_;
+    }
+
+    qglviewer::Frame& frame() {
+        return frame_;
+    }
+
+    // Modifieurs
+    //
+
+    void setMaterial(const Material& material) {
+        material_ = material;
+    }
+
+    void setFrame(const qglviewer::Frame& frame) {
+        frame_ = frame;
+    }
+
+    // Affichage openGL de l'objet
+    //
+    virtual void draw() const = 0;
+    virtual void animate(float time);
+
+    // Rayon d'une sphï¿½re englobante (meme grossiere).
+    //
+    virtual float boundingRadius() const = 0;
+
+    // Intersection method between a Ray and the Object. The resulting
+    // intersection (if any) is stored in the Hit. The associated Frame
+    // allow to transform from local coordinates of this node to global
+    // coordinates.
+    //
+    virtual bool intersect(const Ray&, Hit& hit, qglviewer::Frame frame) const = 0;
+
+    virtual void initFromDOMElement(const QDomElement& e);
+
+    // This operator allow to concatenate two transformations. It performs
+    // the multiplication of the two transformation matrices in the order of
+    // the arguments.
+    //
+    qglviewer::Frame MultiplyFrame(const qglviewer::Frame& f1, const qglviewer::Frame& f2) const;
+
+    std::string& getName() {
+        return name_;
+    }
+
+protected:
+    // Tous les objets ont un repere et un materiau.
+    //
+    qglviewer::Frame frame_;
+    Material material_;
+    std::string name_;
+
+private:
+    bool animateAngle(float time, qglviewer::Quaternion *q);
+    bool animateTranslation(float time, qglviewer::Vec *v);
+
+
 };
